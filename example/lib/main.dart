79be92aa17d026ae8f4f30b95df029d437644967
MohamedAbd0 <mohamed_abdo95@outlook.com>
2022-06-17 17:19:03 +0200

    update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,6 +7,8 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+
+
   @override
   Widget build(BuildContext context) => MaterialApp(
         title: 'Widgets To Image',
